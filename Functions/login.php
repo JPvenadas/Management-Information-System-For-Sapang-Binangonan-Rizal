6498906f2afd93c30af47eff095242e0bc85a5db
Jp Venadas <jaypheevenadas18@gmail.com>
2023-01-28 17:53:26 +0800

    Login Function

--- Functions/login.php
+++ Functions/login.php
@@ -22,37 +22,29 @@ if (isset($_POST['uname']) && isset($_POST['password'])) {
         header("Location: ../index.php?error=Password is required");
 	    exit();
 	}else{
-		$sql = "SELECT u.username, u.password, u.userType, u.verifyToken, pos.posName, r.residentsID, r.firstName,r.middleName, r.lastName, r.image
-		from tbl_useraccounts as u inner JOIN tbl_personnel as p
-		on u.personnelId = p.personnelID 
-		inner join tbl_residents as r 
-		on p.residentsID = r.residentsID
-		inner join tbl_positions as pos 
-		on p.position = pos.posID
-		where u.username = '$uname'";
+		$sql = "SELECT u.userName, u.password, u.userType, p.positionName, r.residentID, r.firstName,r.middleName, r.lastName, r.extension, r.image 
+		from tbl_userAccounts as u INNER JOIN tbl_residents as r on u.residentID = r.residentID
+        inner JOIN tbl_employees as e on r.residentID = e.residentID
+        INNER JOIN tbl_positions as p on e.position = p.positionID
+        WHERE u.userName = '$uname';";
 	
 		$result = mysqli_query($conn, $sql);
 		
 		if (mysqli_num_rows($result) === 1) {
 			$row = mysqli_fetch_assoc($result);
 			mysqli_close($conn);
-
-			if($row['verifyToken'] !== "Verified"){
-				header("Location: ../index.php?error=Your Account needs to be validated");
-				exit();
-			}
 			
-            if ($row['username'] === $uname && password_verify($pass, $row['password'])) {
-            	$_SESSION['username'] = $row['username'];
+            if ($row['userName'] === $uname && password_verify($pass, $row['password'])) {
+            	$_SESSION['username'] = $row['userName'];
 				$_SESSION['userType'] = $row['userType'];
-				$_SESSION['position'] = $row['posName'];
+				$_SESSION['position'] = $row['positionName'];
 				$_SESSION['image'] = $row['image'];
 				$_SESSION['firstName'] = $row['firstName'];
 				$_SESSION['middleName'] = $row['middleName'];
 				$_SESSION['lastName'] = $row['lastName'];
-				$_SESSION['residentsID'] = $row['residentsID'];
-				insertLogs("Logged in");
-            	header("Location: ../Modules/Dashboard/dashboard.php");
+				$_SESSION['extension'] = $row['extension'];
+				$_SESSION['residentsID'] = $row['residentID'];
+            	header("Location: ../Pages/Dashboard/Dashboard.php");
 		        exit();
             }else{
 				header("Location: ../index.php?error=Incorect User name or password");
