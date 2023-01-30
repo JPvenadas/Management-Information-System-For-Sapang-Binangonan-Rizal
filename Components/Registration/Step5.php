244e1328deed3dbbd67cabf82b2c76aeebe0ba49
Jp Venadas <jaypheevenadas18@gmail.com>
2023-01-30 22:36:37 +0800

    registration step 5 (design)

---
+++ Components/Registration/Step5.php
@@ -0,0 +1,21 @@
+<form class="form" action="../../index.php">
+    <div class="limit-width">
+        <h2 class="form-title">Create Password</h2>
+        <p class="text">Your password must be have at least:</p>
+        <ul>
+            <li class="list-item">8 characters long</li>
+            <li class="list-item">1 uppercase & 1 lowercase character</li>
+            <li class="list-item">1 number</li>
+        </ul>
+        <input type="password" class="input" placeholder="Input your Password">
+        <input type="password" class="input" placeholder="Confirm Password">
+    </div>
+    <div class="button-group">
+            <a href="?step=4" class="action-button">
+                <p>Previous</p>
+            </a>
+            <button type="submit" class="action-button">
+                <p>Signup</p>
+            </button>
+    </div>
+</form>
