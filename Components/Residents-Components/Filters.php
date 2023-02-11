0ef2b2c0ff9e7178d20416f9b770e7988c901899
Jp Venadas <jaypheevenadas18@gmail.com>
2023-02-11 10:25:27 +0800

    residents filtering

--- Components/Residents-Components/Filters.php
+++ Components/Residents-Components/Filters.php
@@ -2,22 +2,22 @@
 $verified=""; $unverified="";
     if(isset($_GET['filter'])){
         
-        if($_GET['filter'] == "verified"){
-            $verified = "underline";
-        }else{
+        if($_GET['filter'] == "unverified"){
             $unverified = "underline";
+        }else{
+            $verified = "underline";
         }
 
     }else{
-        $unverified = "underline";
+        $verified = "underline";
     }
 ?>
 
 <div class="action-controls-container">
     <div class="settings-nav">
         <ul class="nav-list">
-            <li><a class="<?php echo $unverified?>" href="?">Unverified Residents</a></li>
-            <li><a class="<?php echo $verified?>" href="?filter=verified">Registered Residents</a></li>
+            <li><a class="<?php echo $verified?>" href="?">Registered Residents</a></li>
+            <li><a class="<?php echo $unverified?>" href="?filter=unverified">Unverified Residents</a></li>
         </ul>
     </div>
     <form action="../../Functions/sql-command-residents.php" method="post" class="search-button-container">
--- Functions/residents-sql-commands.php
+++ Functions/residents-sql-commands.php
@@ -1,12 +1,24 @@
 <?php
 require "db_conn.php";
 
+function addFilters(){
+    $additionalCommands = " and ";
+    if(isset($_GET['filter']) && $_GET['filter'] == "unverified"){
+        $additionalCommands = $additionalCommands . "u.accountStatus = 'Inactive'";
+        return $additionalCommands;
+    }else{
+        $additionalCommands = $additionalCommands . "u.accountStatus = 'Active'";
+        return $additionalCommands;
+    }
+}
+
 function getResidents(){
     $conn = openCon();
-    $command = "SELECT `residentID`,`firstName`,`middleName`,`lastName`,`birthDate`,`image`, p.purokName 
-                FROM tbl_residents INNER JOIN tbl_purok as p 
-                on tbl_residents.Purok = p.purokID Where tbl_residents.archive = 'false'";
-    
+    $command = "SELECT r.residentID,`firstName`,`middleName`,`lastName`,`birthDate`,`image`, p.purokName
+                FROM tbl_residents as r INNER JOIN tbl_purok as p on p.purokID = r.purok
+                INNER JOIN tbl_userAccounts as u on u.residentID = r.residentID
+                WHERE r.archive = 'false'";
+    $command = $command . addFilters(); 
      $result = mysqli_query($conn, $command);
      $residents = mysqli_fetch_all($result, MYSQLI_ASSOC);
      mysqli_free_result($result);
