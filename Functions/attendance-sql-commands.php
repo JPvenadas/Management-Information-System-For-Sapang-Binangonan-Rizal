abf6c4c6354b60726bd9611ec827b20df3e50f89
Jp Venadas <jaypheevenadas18@gmail.com>
2023-03-26 13:18:54 +0800

    filtering of events

--- Functions/attendance-sql-commands.php
+++ Functions/attendance-sql-commands.php
@@ -5,7 +5,7 @@ function getEmployeeAttendance(){
     $conn = openCon();
     $date = date('y/m/d');
     $command = "SELECT a.attendanceID, e.employeeID, r.image ,CONCAT(r.firstName,' ', LEFT(r.middleName,1), '. ', r.lastName, ' ', r.extension) as fullName, e.position, IF(a.date = CURDATE(), a.timeIn, null) as `timeIn`, IF(a.date = CURDATE(), a.timeOut, null) as `timeOut` FROM `tbl_employees` as e
-                LEFT JOIN tbl_attendance as a on a.employeeID = e.employeeID
+                LEFT JOIN tbl_attendance as a on a.employeeID = e.employeeID AND DATE(a.date) = CURDATE()
                 INNER JOIN tbl_residents as r on r.residentID = e.residentID";
     $result = mysqli_query($conn, $command);
     $attendance = mysqli_fetch_all($result, MYSQLI_ASSOC);
--- Functions/events-sql-commands.php
+++ Functions/events-sql-commands.php
@@ -1,8 +1,33 @@
 <?php
 require "db_conn.php";
+
+    function getSelectCommandByFilter(){
+        if(isset($_GET['filter']) and $_GET['filter'] == 'history'){
+            return "SELECT * FROM `tbl_events` WHERE `archive` = 'false' AND `Schedule` < CURDATE()";
+        }else{
+            return "SELECT * FROM `tbl_events` WHERE `archive` = 'false' AND `Schedule` > CURDATE()";
+        }
+    }
+    function searchFilter(){
+        if(isset($_GET['search'])){
+            $search = $_GET['search'];
+            $additionalCommand = " and `eventName` LIKE '%$search%'";
+            return $additionalCommand;
+        }else{
+            return "";
+        }
+    }
+
+    if(isset($_POST['search_button'])){
+        $search = $_POST['search_input'];
+        $filter = $_POST['search_filter'];
+        header("Location: ../../Pages/Events/Events.php?filter=$filter&search=$search");
+        exit();
+    }
     function getEvents(){
         $conn = openCon();
-        $command = "SELECT * FROM `tbl_events` WHERE `archive` = 'false'";
+        $command = getSelectCommandByFilter();
+        $command = $command . searchFilter();
         $result = mysqli_query($conn, $command);
         $events = mysqli_fetch_all($result, MYSQLI_ASSOC);
         mysqli_free_result($result);
