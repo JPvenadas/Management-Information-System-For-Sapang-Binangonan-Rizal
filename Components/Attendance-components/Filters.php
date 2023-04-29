c84c97bcdfdc449fcbc28b0e7455f87634c50cae
Jp Venadas <jaypheevenadas18@gmail.com>
2023-04-29 20:32:09 +0800

    attendance page responsiveness (complete)

--- Components/Attendance-components/Filters.php
+++ Components/Attendance-components/Filters.php
@@ -1,10 +1,10 @@
 <div class="filter-section">
     <div class="left">
-        <input autocomplete="off" id="search" class="filter-input" type="text" placeholder="Filter by Name and Position">
+        <input autocomplete="off" id="search" class="filter-input" type="text" placeholder="Filter by Name or Position">
     </div>
     <form class="right" method="post" action="../../Pages/Attendance/Attendance.php">
         <p>Browse Records from:</p>
-        <div style="display: flex; gap: 15px;">
+        <div class="browse-container">
             <input max="<?php echo date('Y-m-d');?>" name="record_date" type="date">
             <button class="search-button" name="browse_records" type='submit'>
                 <ion-icon name="search"></ion-icon>
